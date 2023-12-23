87602b2a9d9b86379d80c748b36423a4838f3257
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-23 17:19:52 +0100

    add nextItem

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -1,3 +1,13 @@
+import {useCallback, useRef} from "react";
+import {CarouselRef} from "antd/es/carousel";
+
 export function Carousel() {
+    const carouselRef = useRef<CarouselRef | null>(null);
+    const timeoutDuration = useRef<NodeJS.Timeout | null>(null);
+
+    const nextItem = useCallback(() =>  {
+        if(timeoutDuration.current) clearTimeout(timeoutDuration.current);
+        carouselRef?.current?.next();
+    }, []);
 
 }

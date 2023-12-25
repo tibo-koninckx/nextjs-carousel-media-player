124f02890930698b98a7da0e76773c66a31ac68a
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-25 10:22:05 +0100

    add video URLS

--- src/app/components/Videocarousel.tsx
+++ src/app/components/Videocarousel.tsx
@@ -12,6 +12,12 @@ export function Videocarousel() {
         carouselRef?.current?.next();
     }, []);
 
+    const videos = [
+        "https://drive.google.com/file/d/1trIj3AJBv28ZEmvkBNbrWbW16J5fy5Zi/view?usp=sharing",
+        "https://drive.google.com/file/d/1fmd54F0dFEdf97Kx6YCWCSqHiENYKpW5/view?usp=sharing",
+        "https://drive.google.com/file/d/1asHdejSg2uCnKvgqcOfc3Httzl51lT0G/view?usp=sharing"
+    ]
+
         return<>
         <Carousel style={{height: "100vh", overflow: "hidden"}}>
         </Carousel>

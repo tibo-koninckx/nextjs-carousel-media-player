c93ac9d81e61809024a8db8e943c9f44213f3880
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-25 10:35:02 +0100

    add carouselItem

--- src/app/components/Videocarousel.tsx
+++ src/app/components/Videocarousel.tsx
@@ -2,6 +2,7 @@
 import {useCallback, useMemo, useRef} from "react";
 import {CarouselRef} from "antd/es/carousel";
 import {Carousel} from 'antd';
+import {MediaPlayer} from "@/app/components/MediaPlayer";
 
 interface CarouselItem {
     content: React.ReactNode
@@ -11,18 +12,27 @@ export function Videocarousel() {
     const carouselRef = useRef<CarouselRef | null>(null);
     const timeoutDuration = useRef<NodeJS.Timeout | null>(null);
 
-    const nextItem = useCallback(() =>  {
-        if(timeoutDuration.current) clearTimeout(timeoutDuration.current);
+    const nextItem = useCallback(() => {
+        if (timeoutDuration.current) clearTimeout(timeoutDuration.current);
         carouselRef?.current?.next();
     }, []);
 
     const videos = [
-        "https://drive.google.com/file/d/1trIj3AJBv28ZEmvkBNbrWbW16J5fy5Zi/view?usp=sharing",
-        "https://drive.google.com/file/d/1fmd54F0dFEdf97Kx6YCWCSqHiENYKpW5/view?usp=sharing",
-        "https://drive.google.com/file/d/1asHdejSg2uCnKvgqcOfc3Httzl51lT0G/view?usp=sharing"
+        {id: 1, url: "https://drive.google.com/file/d/1trIj3AJBv28ZEmvkBNbrWbW16J5fy5Zi/view?usp=sharing"},
+        {id: 2, url: "https://drive.google.com/file/d/1fmd54F0dFEdf97Kx6YCWCSqHiENYKpW5/view?usp=sharing"},
+        {id: 3, url: "https://drive.google.com/file/d/1asHdejSg2uCnKvgqcOfc3Httzl51lT0G/view?usp=sharing"}
     ]
 
-        return<>
+    const carouselItem: CarouselItem[] = useMemo(() => {
+        return [
+            ...videos.map((media, index) => {
+                return {content: <MediaPlayer key={media.id} videoURL={media.url} onNext={nextItem}/>};
+            }),
+        ];
+    }, [videos, nextItem]);
+
+
+    return <>
         <Carousel style={{height: "100vh", overflow: "hidden"}}>
         </Carousel>
     </>

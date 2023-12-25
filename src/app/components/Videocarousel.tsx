3d2aa1bb4fd2807a5061eeffebf852cf029df487
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-25 10:54:52 +0100

    add videos to carousel

--- src/app/components/Videocarousel.tsx
+++ src/app/components/Videocarousel.tsx
@@ -17,13 +17,14 @@ export function Videocarousel() {
         carouselRef?.current?.next();
     }, []);
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const videos = [
         {id: 1, url: "https://drive.google.com/file/d/1trIj3AJBv28ZEmvkBNbrWbW16J5fy5Zi/view?usp=sharing"},
         {id: 2, url: "https://drive.google.com/file/d/1fmd54F0dFEdf97Kx6YCWCSqHiENYKpW5/view?usp=sharing"},
         {id: 3, url: "https://drive.google.com/file/d/1asHdejSg2uCnKvgqcOfc3Httzl51lT0G/view?usp=sharing"}
     ]
 
-    const carouselItem: CarouselItem[] = useMemo(() => {
+    const videoItem: CarouselItem[] = useMemo(() => {
         return [
             ...videos.map((media, index) => {
                 return {content: <MediaPlayer key={media.id} videoURL={media.url} onNext={nextItem}/>};
@@ -34,6 +35,11 @@ export function Videocarousel() {
 
     return <>
         <Carousel style={{height: "100vh", overflow: "hidden"}}>
+            {videoItem.map((item, index) => {
+                return <div key={index} style={{ width: "100%", height: "100%", padding: "10px" }}>
+                    {item.content}
+                </div>
+            })}
         </Carousel>
     </>
 

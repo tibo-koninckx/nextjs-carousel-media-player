237879177d4e70ad1776d09b961f2747f5026de2
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-12 10:32:46 +0100

    bugfix carrousel media-player

--- src/app/components/MediaPlayer.tsx
+++ src/app/components/MediaPlayer.tsx
@@ -1,36 +1,40 @@
 import ReactPlayer from "react-player";
-import {useEffect, useState} from "react";
+import {Key, useEffect, useRef, useState} from "react";
 
 interface MediaPlayerProps {
     onNext: () => void;
     onVideoReady: () => void;
     videoURL: string;
     shouldStart: boolean;
+    key?: Key | null | undefined;
 }
 
 export function MediaPlayer(props: MediaPlayerProps) {
-    const { videoURL, onNext, onVideoReady, shouldStart } = props;
-    const [playing, setPlaying] = useState(false);
-
-    const videoEnd = () => {
-        setPlaying(false);
-        onNext();
-    };
+    const { videoURL, onNext, onVideoReady, shouldStart, key } = props;
+    const playerRef = useRef<ReactPlayer>(null);
 
     useEffect(() => {
         if (shouldStart) {
-            setPlaying(true);
+            playerRef.current?.seekTo(0);
         }
     }, [shouldStart]);
 
+    const onDone = () => {
+        console.log(playerRef.current?.getCurrentTime())
+        playerRef.current?.seekTo(0);
+        onNext();
+    }
+
     return (
         <ReactPlayer
+            key={key}
+            ref={playerRef}
             url={videoURL}
-            playing={playing}
+            playing={shouldStart}
             muted={true}
             height={'100vh'}
             width={'100vw'}
-            onEnded={videoEnd}
+            onEnded={onDone}
         />
     );
 }
--- src/app/components/Videocarousel.tsx
+++ src/app/components/Videocarousel.tsx
@@ -1,62 +1,41 @@
 'use client'
 import {useCallback, useEffect, useMemo, useRef, useState} from "react";
-import { CarouselRef } from "antd/es/carousel";
-import { Carousel } from 'antd';
-import { MediaPlayer } from "@/app/components/MediaPlayer";
-
-interface CarouselItem {
-    content: React.ReactNode
-}
+import {CarouselRef} from "antd/es/carousel";
+import {Carousel} from 'antd';
+import {MediaPlayer} from "@/app/components/MediaPlayer";
 
 export function Videocarousel() {
     const carouselRef = useRef<CarouselRef | null>(null);
     const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
-    const [shouldStartVideo, setShouldStartVideo] = useState(false);
-    const [nextVideoIndex, setNextVideoIndex] = useState(0);
+
+
+    const nextItem = () => {
+        setCurrentVideoIndex((currentVideoIndex + 1) % videos.length);
+    };
 
     useEffect(() => {
-        if (shouldStartVideo) {
-            carouselRef?.current?.next();
-            setCurrentVideoIndex(nextVideoIndex);
-            setShouldStartVideo(false);
-            if(nextVideoIndex === videos.length) {
-                setNextVideoIndex(0);
-            }
-        }
-    }, [shouldStartVideo, nextVideoIndex]);
-
-    const nextItem = useCallback(() => {
-        setNextVideoIndex((currentIndex) => (currentIndex + 1) % videos.length);
-        setShouldStartVideo(true);
-    }, []);
-
+        console.log("Next video", currentVideoIndex)
+        carouselRef.current?.goTo(currentVideoIndex);
+    }, [currentVideoIndex]);
 
 
     const handleVideoReady = useCallback(() => {
-        setShouldStartVideo(true);
+        // setShouldStartVideo(true);
     }, []);
 
     const videos = [
-        { id: 1, url: 'https://www.youtube.com/watch?v=zU9y354XAgM' },
-        { id: 2, url: 'https://youtu.be/xuP4g7IDgDM' },
-        { id: 3, url: 'https://youtu.be/J1qsrBl_CR0' }
+        {id: 2, url: 'https://youtu.be/xuP4g7IDgDM'},
+        {id: 1, url: 'https://www.youtube.com/watch?v=zU9y354XAgM'},
+        {id: 3, url: 'https://youtu.be/J1qsrBl_CR0'}
     ];
 
-    const videoItem: CarouselItem[] = useMemo(() => {
-        return [
-            ...videos.map((media, index) => {
-                return { content: <MediaPlayer key={shouldStartVideo ? 'playing' : 'paused'} videoURL={media.url} onVideoReady={handleVideoReady} shouldStart={currentVideoIndex ===index} onNext={nextItem} /> };
-            }),
-        ];
-    }, [videos, handleVideoReady, currentVideoIndex, shouldStartVideo]);
-
     return (
-        <Carousel style={{ height: "100vh", overflow: "hidden" }} ref={carouselRef}>
-            {videoItem.map((item, index) => (
-                <div key={index} style={{ width: "100%", height: "100%", padding: "10px" }}>
-                    {item.content}
-                </div>
-            ))}
+        <Carousel style={{height: "100vh", overflow: "hidden"}} ref={carouselRef}>
+            {
+                videos.map((media, index) => {
+                    return <MediaPlayer key={index} videoURL={media.url} onVideoReady={handleVideoReady} shouldStart={index === currentVideoIndex} onNext={nextItem}/>;
+                })
+            }
         </Carousel>
     );
 }
